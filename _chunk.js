cb322d16d23bcb625a31aa5b0ea6b16aaf97c0f5
Joseph Jin <jinyuhui61@hotmail.com>
2018-10-03 10:51:31 -0700

    _chunk

---
+++ _chunk.js
@@ -0,0 +1,26 @@
+/**
+ * Chunck
+ * @method _chunk()
+ * @param array
+ * @param int size (optional, default 0)
+ * @return array
+ */
+
+function chunk(arr, size) {
+  if (isNaN(size) || size < 1) {
+    return arr;
+  }
+  size -= 0
+  var result = [];
+  for (var i = 0; i < arr.length; i = i + size) {
+    var chunk = [];
+    for (var j = 0; j < size; j++) {
+      var index = i + j;
+      if (index < arr.length) {
+        chunk.push(arr[index]);
+      }
+    }
+    result.push(chunk);
+  }
+  return result;
+}
